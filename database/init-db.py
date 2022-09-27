30a3e21e6ddff762282d6540ac3d95d9ce5587e8
Jiahao Ji Zhou <jiahaojizh@gmail.com>
2022-09-27 08:37:53 +0000

    Set database types and set username as PK

--- database/init-db.py
+++ database/init-db.py
@@ -5,7 +5,10 @@ DB_NAME = "birthday.db"
 def createDB():
     conn = sql.connect(DB_NAME)
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE birthday(username, birthday)")
+    cursor.execute("""CREATE TABLE birthday(
+        username TEXT PRIMARY KEY,
+        birthday TEXT NOT NULL
+        )""")
     conn.commit()
     conn.close()
 
