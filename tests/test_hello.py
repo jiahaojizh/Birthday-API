dc4bbeb71fb82e3881cef714c20b4e13658b4d22
Jiahao Ji Zhou <jiahaojizh@gmail.com>
2022-09-29 20:30:18 +0000

    Adapt the tests to support any port

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -11,8 +11,9 @@ from flaskr import hello
 TODAY = datetime.now()
 TODAY_ISO = date(TODAY.year, TODAY.month, TODAY.day).isoformat()
 
-HOST = "172.17.0.2"
+HOST = "localhost"
 USER = "newuser"
+PORT = "80"
 
 ### DB CONNECTION
 def test_db_connection():
@@ -21,23 +22,23 @@ def test_db_connection():
 
 ### POST METHOD
 def test_post_user():
-    r = requests.put('http://' + HOST + ':5000/hello/' + USER, data={'dateOfBirth': '1998-01-01'})
+    r = requests.put('http://' + HOST + ':' + PORT + '/hello/' + USER, data={'dateOfBirth': '1998-01-01'})
     assert r.status_code == 204
     assert r.text == ''
-    requests.delete('http://' + HOST + ':5000/hello/' + USER)
+    requests.delete('http://' + HOST + ':' + PORT + '/hello/' + USER)
 
 def test_post_user_with_invalid_username():
-    r = requests.put('http://' + HOST + ':5000/hello/n3w-us4r', data={'dateOfBirth': '1998-01-01'})
+    r = requests.put('http://' + HOST + ':' + PORT + '/hello/n3w-us4r', data={'dateOfBirth': '1998-01-01'})
     assert r.status_code == 400
     assert r.json()["message"] == "The username can only contain letters, received: n3w-us4r"
 
 def test_post_user_wrong_date():
-    r = requests.put('http://' + HOST + ':5000/hello/' + USER, data={'dateOfBirth': '2050-01-01'})
+    r = requests.put('http://' + HOST + ':' + PORT + '/hello/' + USER, data={'dateOfBirth': '2050-01-01'})
     assert r.status_code == 400
     assert r.json()["message"] == "The field dateOfBirth must be before than today (" + TODAY_ISO + "), data received: 2050-01-01"
 
 def test_post_user_incorrect_date_format():
-    r = requests.put('http://' + HOST + ':5000/hello/' + USER, data={'dateOfBirth': '1998-00-00'})
+    r = requests.put('http://' + HOST + ':' + PORT + '/hello/' + USER, data={'dateOfBirth': '1998-00-00'})
     assert r.status_code == 400
     assert r.json()["message"] == "The field dateOfBirth expects to receive a correct date with the format %YYYY-%MM-%DD, data received: 1998-00-00"
 
@@ -47,22 +48,22 @@ def test_get_user():
     day = 27
     if day == TODAY.day:
         day = 12
-    r = requests.put('http://' + HOST + ':5000/hello/' + USER, data={'dateOfBirth': date(TODAY.year, TODAY.month, day).isoformat()})
+    r = requests.put('http://' + HOST + ':' + PORT + '/hello/' + USER, data={'dateOfBirth': date(TODAY.year, TODAY.month, day).isoformat()})
     assert r.status_code == 204
-    r = requests.get('http://' + HOST + ':5000/hello/' + USER)
+    r = requests.get('http://' + HOST + ':' + PORT + '/hello/' + USER)
     assert r.status_code == 200
     assert "Hello, " + USER + "! Your birthday is in" in r.json()["message"]
-    requests.delete('http://' + HOST + ':5000/hello/' + USER)
+    requests.delete('http://' + HOST + ':' + PORT + '/hello/' + USER)
 
 def test_get_user_with_birthday():
-    r = requests.put('http://' + HOST + ':5000/hello/' + USER, data={'dateOfBirth': date(TODAY.year-20, TODAY.month, TODAY.day).isoformat()})
-    r = requests.get('http://' + HOST + ':5000/hello/' + USER)
+    r = requests.put('http://' + HOST + ':' + PORT + '/hello/' + USER, data={'dateOfBirth': date(TODAY.year-20, TODAY.month, TODAY.day).isoformat()})
+    r = requests.get('http://' + HOST + ':' + PORT + '/hello/' + USER)
     assert r.status_code == 200
     assert r.json()["message"] == "Hello, " + USER + "! Happy birthday!"
-    requests.delete('http://' + HOST + ':5000/hello/' + USER)
+    requests.delete('http://' + HOST + ':' + PORT + '/hello/' + USER)
 
 def test_get_non_existent_user():
-    r = requests.get('http://' + HOST + ':5000/hello/nonexistentuser')
+    r = requests.get('http://' + HOST + ':' + PORT + '/hello/nonexistentuser')
     assert r.status_code == 404
     assert r.json()["message"] == "Error 404. The user with username: 'nonexistentuser' does not exist"
 
